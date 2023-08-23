ab9ca7b1fc893549fcc9d18857f9a34ef523c494
Code Frost <simon@penguinpowered.co.uk>
2023-08-23 21:20:29 +0100

    Update BackupData.ps1

--- BackupData.ps1
+++ BackupData.ps1
@@ -1,10 +1,10 @@
 #!/usr/bin/pwsh
 
 # Source directory to be backed up
-$sourceDir = "/media/penguinpoweredapps/Data"
+$sourceDir = "/source-directory"
 
 # Destination directory for backup
-$destinationDir = "/media/penguinpoweredapps/27244a8a-6caa-43f0-9dc6-130d933a3be1/Backup/Data"
+$destinationDir = "/destination-directory"
 
 # Execute the rsync command to perform the backup
 rsync -av --progress $sourceDir $destinationDir
