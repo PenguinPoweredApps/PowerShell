75573d81b6d30b7fbc404bc441762ebea748bf2a
Penguin Powered Apps <simon@penguinpowered.co.uk>
2024-02-06 09:26:53 +0000

    Added new files

--- CreateScheduledTask.ps1
+++ CreateScheduledTask.ps1
@@ -4,4 +4,7 @@ $trigger = New-ScheduledTaskTrigger -AtStartup
 # $trigger = New-ScheduledTaskTrigger -AtLogOn
 # $trigger = New-ScheduledTaskTrigger -At 3pm -Daily
 
-Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "MyTask"
+# Define the principal with highest privileges
+$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+
+Register-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -TaskName "MyTask"
---
+++ RunAQuickVirusScan.ps1
@@ -0,0 +1,1 @@
+start-mpscan -scantype quickscan
