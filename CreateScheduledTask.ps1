a3add0eab2215be3d475abda5621cc1223fa1bf4
Penguin Powered Apps <simon@penguinpowered.co.uk>
2024-02-06 07:30:09 +0000

    Added 1 new file edited 1 existing file

--- CreateScheduledTask.ps1
+++ CreateScheduledTask.ps1
@@ -1,3 +1,7 @@
-$action = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "YourScript.ps1"
+$action = New-ScheduledTaskAction -Execute "pwsh.exe" -Argument "C:\Scripts\YourScript.ps1"
+
 $trigger = New-ScheduledTaskTrigger -AtStartup
+# $trigger = New-ScheduledTaskTrigger -AtLogOn
+# $trigger = New-ScheduledTaskTrigger -At 3pm -Daily
+
 Register-ScheduledTask -Action $action -Trigger $trigger -TaskName "MyTask"
